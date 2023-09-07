567550dc3ef5e70d26caab8800bd3d84fad33ef8
eddiek2 <edwinkwao2@gmail.com>
2023-09-06 20:30:14 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - output the name of an integer
+ * @argc: argument number
+ * @argv: argument array
+ * Return: always return 0
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
