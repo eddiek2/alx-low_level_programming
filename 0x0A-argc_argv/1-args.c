62926b2f7df345771c178e28e094d4d0825f58a4
eddiek2 <edwinkwao2@gmail.com>
2023-09-24 07:07:11 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,13 +5,13 @@
  * main -print argument in program
  * @argc: argument number
  * @argv: argument array
- * Return: Always return 0
+ * Return: Always return 0 (Success)
  */
 
 int main(int argc, char *argv[])
 {
 	(void) argv; /*Ignore argv*/
-	printf("%d\n", argc - 1);
+		printf("%d\n", argc - 1);
 
 	return (0);
 }
