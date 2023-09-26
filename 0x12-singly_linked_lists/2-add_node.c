df31faff356bfafd72aedae99ac1d19fcc85f62a
eddiek2 <edwinkwao2@gmail.com>
2023-09-26 05:43:44 -0700

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -5,24 +5,24 @@
 /**
  * add_node - a function that adds new node
  * @head: the head of the node
- * #str:string to the store
+ * @str:string to the store
  * Return: address of new elements
  */
 
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *nwnode;
+	list_t *newnode;
 	unsigned int k, count = 0;
 
-	nwnode = malloc(sizeof(list_t));
-	if (nwnode == NULL)
+	newnode = malloc(sizeof(list_t));
+	if (newnode == NULL)
 		return (NULL);
-	nwnode->str = strdup(str);
+	newnode->str = strdup(str);
 	for (k = 0; str[k] != '\0'; k++)
 		count++;
-	nwnode->len = count;
-	nwnode->next = *head;
-	*head = nwnode;
+	newnode->len = count;
+	newnode->next = *head;
+	*head = newnode;
 
 	return (*head);
 }
